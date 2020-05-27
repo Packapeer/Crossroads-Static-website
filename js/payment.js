53797fad05e60f8ead53f6c021387061e7c482ca
Shabeersha <shabeersha786786@gmail.com>
2020-05-28 03:04:49 +0530

    payment process integrated with razorpay

---
+++ js/payment.js
@@ -0,0 +1,35 @@
+
+
+function paymentprocess(){
+
+
+    var options = {
+        "key": "rzp_test_EjwEX0eJ90FIOx", // Enter the Key ID generated from the Dashboard
+        "amount": 5000*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        "currency": "INR",
+        "name": "CROSSROADS",
+        "description": "Exclusive Membership For One Year",
+        "image": "images/main-logo-crossroads.jpg",
+        //"order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        "handler": function (response){
+
+            $('#form-modal').modal('toggle');
+            alert(response.razorpay_payment_id);
+            alert(response.razorpay_order_id);
+            alert(response.razorpay_signature)
+        },
+        "prefill": {
+            "name": "",
+            "email": "",
+            "contact": ""
+        },
+        "notes": {
+            "address": ""
+        },
+        "theme": {
+            "color": "#000"
+        }
+    };
+    var propay = new Razorpay(options);
+    propay.open();
+}
