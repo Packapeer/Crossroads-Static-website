daa37b3b910a9c91bf995a69ca0ebc42cdb97d1c
Shabeersha <shabeersha786786@gmail.com>
2020-06-03 12:56:51 +0530

    google ads conversion tracking ready for verification

--- js/payment.js
+++ js/payment.js
@@ -13,9 +13,13 @@ function paymentprocess(){
         //"order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
         "handler": function (response){
 
-            conversion();
+            // conversion();
 
-            $('#form-modal').modal('toggle');
+            window.location = "../form/a6a2729cbf6bcadce577a31f7f76201d5ce63c57d6c53318000d67714bb354ef.html";
+
+            
+
+            
             // alert(response.razorpay_payment_id);
             // alert(response.razorpay_order_id);
             // alert(response.razorpay_signature)
@@ -36,6 +40,6 @@ function paymentprocess(){
     propay.open();
 }
 
-function conversion(){
-    ga('button','click');
-}
+// function conversion(){
+//     ga('button','click');
+// }
